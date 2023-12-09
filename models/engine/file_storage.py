cdef492a1fe93eecc3a9ac4ca681c257f2d72ca9
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-09 17:56:29 +0200

    Build the structure of file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from json import loads, dumps
+
+
+class FileStorage:
+
+    __file_path = ""
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+    
+    def new(self, obj):
+        pass
+
+    def save(self):
+        pass
+
+    def reload(self):
+        pass
