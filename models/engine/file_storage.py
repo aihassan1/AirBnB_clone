77e2ca9f3980adb7a7d8a5d0a5576ab6ee4cc8ce
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-09 18:18:23 +0200

    Complete documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
 
+"""Define FileStorage class that that make the projects persistent"""
 from json import loads, dumps
 
 
 class FileStorage:
+    """
+        FileStorage class
 
-    __file_path = ""
+        Attributes:
+            __file_path (string): path to the JSON file
+            __objects (dictionary): empty - storeobjects by <class name>.id
+
+        Methods:
+        all - new - save - reload
+    """
+    __file_path = "AirBnB_Storage.json"
     __objects = {}
 
     def all(self):
+        "return __objects"
         return self.__objects
-    
+
     def new(self, obj):
+        """set new obj inside __objects"""
         pass
 
     def save(self):
+        """save __objects in __file_path"""
         pass
 
     def reload(self):
+        """load __objects from __file_path"""
         pass
