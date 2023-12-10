268f41e1961b83badee17477ea17687f25cf3ea0
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-10 20:54:53 +0200

    clean BnB

--- models/State.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Defines the State class, inheriting from BaseModel """
-
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """State class represents a geographical state"""
-    name = ''
