ce334cb1bccad4e78a0f49bf05c05b58bd88c51c
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-10 19:44:48 +0200

    file storage unittests completed

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 from models import storage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
 
 all_objs = storage.all()
-
+print(type(all_objs))
+print(len(all_objs))
+print("I am here can you do that pleeeeeeeeeeeeeease only for me")
+GGpro = all_objs.values()
+print(f"MY type is ... {type(GGpro)}")
+print(GGpro)
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
