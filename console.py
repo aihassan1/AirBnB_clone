68a24b5c2a81ee4e7e8a04752bf47937f21fd473
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-11 03:12:03 +0200

    Complete console

--- console.py
+++ console.py
@@ -40,6 +40,7 @@ class HBNBCommand(cmd.Cmd):
             obj = HBNBCommand.bnb_cls[cls_name]()
             print(obj.id)
             storage.save()
+
     def do_show(self, line):
         """
             Prints the string representation
@@ -83,7 +84,68 @@ class HBNBCommand(cmd.Cmd):
             if obj is None:
                 print("** no instance found **")
             else:
-                print(obj)
+                del storage.all()[inst_key]
+                storage.save()
+
+    def do_all(self, line):
+        """
+        Prints all string representation of all instances based
+        or not on the class name.
+        """
+        args = self.parseline(line)
+        cls_name = args[0]
+        str_inst = []
+
+        if cls_name is None:
+            for K, ob in storage.all().items():
+                str_inst.append(ob.__str__())
+            print(str_inst)
+        elif cls_name not in HBNBCommand.bnb_cls:
+            print("** class doesn't exist **")
+        else:
+            for K, ob in storage.all().items():
+                if K.startswith(cls_name):
+                    str_inst.append(ob.__str__())
+            print(str_inst)
+        
+
+        def do_update(self, line):
+            """
+            Updates an instance based on the class name and id by adding
+            or updating attribute (save the change into the JSON file).
+            """
+        args = self.parseline(line)
+        cls_name = args[0]
+        inst_id = args[1]
+
+        if cls_name is None:
+            print("** class name missing **")
+        elif cls_name not in HBNBCommand.bnb_cls:
+            print("** class doesn't exist **")
+        elif inst_id is None or id == "":
+            print("** instance id missing **")
+        else:
+            inst_key = f"{cls_name}.{inst_id}"
+            obj = storage.all()[inst_key]
+            att_name = args[2]
+            att_value = args[3]
+
+            if obj is None:
+                print("** no instance found **")
+            else:
+                inst_key = f"{cls_name}.{inst_id}"
+                obj = storage.all().get(inst_key)
+
+                if obj is None:
+                    print("** no instance found **")
+                elif att_name is None:
+                    print("** attribute name missing **")
+                elif att_value is None:
+                    print("** value missing **")
+                else:
+                    setattr(obj, att_name, att_value)
+                    # self updated at date time
+                    storage.save()
 
 
 if __name__ == '__main__':
