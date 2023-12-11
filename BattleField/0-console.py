f03e42ffcb32011cf816b83ee7308eb31b9ed21e
aihassan1 <abdosruor57@gmail.com>
2023-12-11 02:35:26 +0000

    pycode

--- BattleField/0-console.py
+++
@@ -1,8 +0,0 @@
-class MyClass:
-    def __init__(self):
-        self.message = "Hello from MyClass"
-
-class_name = MyClass
-new_instance = class_name()
-
-print(new_instance.message)
--- models/place.py
+++ models/place.py
@@ -3,7 +3,7 @@
 
 from models.base_model import BaseModel
 
-    
+
 class Place(BaseModel):
     """Represents a Amenity with name attributes"""
     city_id = ''
