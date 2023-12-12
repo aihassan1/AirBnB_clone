03f0eb9824db51839eeff0e842d4c73bd89c0bca
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-12 16:33:35 +0200

    completed

--- Training/3-Cmd_test_update_dict.py
+++ Training/3-Cmd_test_update_dict.py
@@ -29,10 +29,10 @@ class HBNBCommand(Cmd):
         args[2] = args[2][start: end]
 
         #FUNCTION CLASS ID
-        cls_id = "#" + args[0] + "#" + args[2]
+        cls_id = args[0] + " " + args[2] + " "
 
         for att, val in args[3].items():
-            update_formate = cls_id + "#" + str(att) + "#" + '\"'+ str(val) +'\"'
+            update_formate = cls_id + str(att) + " " + '\"' + str(val) +'\"'
             # You can see WooooOOOOOOOOOooooW Format here
             print(f"WOOOOW format: {update_formate}")
 
